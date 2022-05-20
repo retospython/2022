71354306c8e1cfc5e68c4599bc5f5d614be3c8cf
retospython <105950130+retospython@users.noreply.github.com>
2022-05-20 16:37:53 -0500

    Create reto2.py

---
+++ reto2.py
@@ -0,0 +1,13 @@
+
+alto=  float(input())
+ancho= float(input())
+profundo= float(input())
+volumen= (alto*ancho*profundo)
+costo= volumen*5
+if alto>30:
+    costo=costo+2000
+if costo>10000:
+    costo=costo+(costo*0.19)
+
+print(volumen)
+print(costo)
